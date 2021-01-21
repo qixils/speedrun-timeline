d89812e9c7e21e08583993eeaa0ca89d153bd0b2
Lexi Larkin <noellekiq@gmail.com>
2021-01-20 19:46:05 -0500

    remove todo

--- scripts/src-collector.py
+++ scripts/src-collector.py
@@ -242,7 +242,6 @@ def main():
 
     out = {x: None for x in ['date']+list(runner_dict.keys())}
 
-    # TODO: include game name and category in filename
     file_re = re.compile(r"[^A-Za-z0-9-_ ]")
     with open(file_re.sub("_", f"{game['names']['international']}-{category['name']}")+'.csv', 'w', newline='') as f:
         w = csv.DictWriter(f, fieldnames=out.keys(), quoting=csv.QUOTE_NONE)
